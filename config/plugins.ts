e0fc5ffa6ed4c6aafe7f00a2c341c4384dc99499
tomahawwk <t.ahawwk@gmail.com>
2023-12-20 03:39:10 +0100

    upd

--- config/plugins.ts
+++ config/plugins.ts
@@ -9,5 +9,34 @@ module.exports = ({ env }) => ({
           
         },
     },
+    "strapi-plugin-populate-deep": {
+		config: {
+			defaultDepth: 5,
+		},
+	},
+    io: {
+		enabled: true,
+		config: {
+			IOServerOptions: {
+				cors: { origin: "http://localhost:3001", methods: ["GET", "POST"] },
+			},
+			contentTypes: {
+				message: "*",
+				chat: ["create"],
+			},
+			events: [
+				{
+					name: "connection",
+					handler: ({ strapi }, socket) => {
+						strapi.log.info(`[io] new connection with id ${socket.id}`);
+
+						socket.on("client-message", async (messageData) => {
+							strapi.$io.raw("server-message", messageData);
+						});
+					},
+				},
+			],
+		},
+	},
 },
 });
